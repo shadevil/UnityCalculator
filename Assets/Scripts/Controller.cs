4468834c892dbfb54b9fd86f1b2b314ef265a544
shadevil <linkinfan@bk.ru>
2022-07-09 19:35:46 +0500

    Пофиксил все анимации(все кнопки анимированы вне зависимости от включения), убрал вкл/выкл звуки(теперь все звуки нажатий случайные из списка), при делении на ноль сделал невозможной ошибку неверных данных,добавил цвет и анимацию при ошибке

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -16,21 +16,6 @@ public class Controller : MonoBehaviour
     [SerializeField] private AudioSource clickSound;
     [SerializeField] private AudioSource errorSound;
 
-    [SerializeField] private Animator _onAnimator;
-    [SerializeField] private Animator _offAnimator;
-    [SerializeField] private Animator _equalsAnimator;
-    [SerializeField] private Animator _cAnimator;
-    [SerializeField] private Animator _pointAnimator;
-    [SerializeField] private Animator _oneAnimator;
-    [SerializeField] private Animator _twoAnimator;
-    [SerializeField] private Animator _threeAnimator;
-    [SerializeField] private Animator _fourAnimator;
-    [SerializeField] private Animator _fiveAnimator;
-    [SerializeField] private Animator _sixAnimator;
-    [SerializeField] private Animator _sevenAnimator;
-    [SerializeField] private Animator _eightAnimator;
-    [SerializeField] private Animator _nineAnimator;
-    [SerializeField] private Animator _zeroAnimator;
 
     void Start()
     {
@@ -46,18 +31,29 @@ public class Controller : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 20))
             {
+                #region Проверка на попадание в кнопку
+                if (hit.transform.IsChildOf(transform))
+                {
+                    if (hit.transform.tag != Names.On)
+                    {
+                        Animator anim = hit.transform.GetComponentInParent<Animator>();
+                        anim.SetTrigger(Names.Click);
+                        clickSound.Play();
+                    }
+                    else onSound.Play();
+                    //if (hit.transform.tag != Names.Off) clickSound.Play(); else offSound.Play();
+                }
+                #endregion
+
                 if (display.activeSelf)
                 {
                     if (hit.transform.tag == Names.Off)
                     {
-                        offSound.Play();
-                        _offAnimator.SetTrigger(Names.Click);
                         display.SetActive(false);
 
                     }
                     if (hit.transform.tag == Names.Numb && displayText.text.Length < 7)
-                    {
-                        clickSound.Play();
+                    {                        
                         if (!isFloat) displayText.text = (displayText.text == "0") ? hit.transform.name : (displayText.text + hit.transform.name);
                         else displayText.text = (displayText.text == "0") ? "0." + hit.transform.name : (displayText.text + hit.transform.name);
                         isFloat = false;
@@ -65,15 +61,12 @@ public class Controller : MonoBehaviour
 
                     if (hit.transform.tag == Names.Add || hit.transform.tag == Names.Subtract || hit.transform.tag == Names.Multiply || hit.transform.tag == Names.Divide)
                     {
-                        clickSound.Play();
                         param = hit.transform.tag;
                         first_numb = float.Parse(displayText.text);
                         displayText.text = "0";
                     }
                     if (hit.transform.tag == Names.Calculate)
                     {
-                        clickSound.Play();
-                        _equalsAnimator.SetTrigger(Names.Click);
                         second_numb = float.Parse(displayText.text);
 
                         displayText.text = Calculator.Calculate(first_numb, second_numb, param, out int Error).ToString();
@@ -86,62 +79,20 @@ public class Controller : MonoBehaviour
 
                     if (hit.transform.tag == Names.Point)
                     {
-                        clickSound.Play();
-                        _pointAnimator.SetTrigger(Names.Click);
                         isFloat = true;
                         intPart = float.Parse(displayText.text);
                     }
 
                     if (hit.transform.tag == Names.C)
                     {
-                        _cAnimator.SetTrigger(Names.Click);
                         displayText.text = "0";
                         isFloat = false;
-                        clickSound.Play();
-                    }
-
-                    
-                    //    switch (hit.transform.tag)
-                    //{
-                    //    case "1b":
-                    //        _oneAnimator.SetTrigger(Names.Click);
-                    //        break;
-                    //    case "2b":
-                    //        _twoAnimator.SetTrigger(Names.Click);
-                    //        break;
-                    //    case "3b":
-                    //        _threeAnimator.SetTrigger(Names.Click);
-                    //        break;
-                    //    case "4b":
-                    //        _fourAnimator.SetTrigger(Names.Click);
-                    //        break;
-                    //    case "5b":
-                    //        _fiveAnimator.SetTrigger(Names.Click);
-                    //        break;
-                    //    case "6b":
-                    //        _sixAnimator.SetTrigger(Names.Click);
-                    //        break;
-                    //    case "7b":
-                    //        _sevenAnimator.SetTrigger(Names.Click);
-                    //        break;
-                    //    case "8b":
-                    //        _eightAnimator.SetTrigger(Names.Click);
-                    //        break;
-                    //    case "9b":
-                    //        _nineAnimator.SetTrigger(Names.Click);
-                    //        break;
-                    //    //case "0b":
-                    //    //    _zeroAnimator.SetTrigger(Names.Click);
-                    //    //    break;
-                    //}
-                    
+                    }                    
                 }
                 else 
                 {
                     if(hit.transform.tag == Names.On) 
                     {
-                        onSound.Play();
-                        _onAnimator.SetTrigger(Names.Click);
                         display.SetActive(true);
                         displayText.text = "0";
                     }
